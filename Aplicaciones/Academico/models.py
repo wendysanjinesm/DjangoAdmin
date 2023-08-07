ccd1cdfd5dbf5328ea2f66040d91116eae32e9b4
wendysanjinesm <wendy.sanjines.m@gmail.com>
2023-08-06 22:36:58 -0400

    permisos

--- Aplicaciones/Academico/models.py
+++ Aplicaciones/Academico/models.py
@@ -5,6 +5,11 @@ class Curso(models.Model):
     codigo=models.CharField(primary_key=True,max_length=6)
     nombre=models.CharField(max_length=1000)
     creditos=models.PositiveSmallIntegerField()
+    class Meta:
+        permissions = [
+            ("change_task_status", "Can change the status of tasks"),
+            ("close_task", "Can remove a task by setting its status as closed"),
+        ]
 
     def _str_(self):
         texto="{0} ({1})"
