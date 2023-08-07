096194db22e585768ea561f2f8345a315b2970b4
wendysanjinesm <wendy.sanjines.m@gmail.com>
2023-08-07 14:49:42 -0400

    clases

--- Aplicaciones/Academico/admin.py
+++ Aplicaciones/Academico/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Curso
+from .models import Curso, Event, Venue, MyClubUser
 
 # Register your models here.
 admin.site.register(Curso)
+admin.site.register(Venue)
+admin.site.register(MyClubUser)
+admin.site.register(Event)
--- Aplicaciones/Academico/models.py
+++ Aplicaciones/Academico/models.py
@@ -11,7 +11,35 @@ class Curso(models.Model):
             ("close_task", "Can remove a task by setting its status as closed"),
         ]
 
-    def _str_(self):
+    def __str__(self):
         texto="{0} ({1})"
         return texto.format(self.nombre,self.creditos)
+
+class Venue(models.Model):
+    name = models.CharField( 'Venue Name', max_length=120 )
+    address = models.CharField( max_length=300 )
+    zip_code =  models.CharField( 'Zip Code', max_length=120 )
+    phone =  models.CharField( 'Contact Phone', max_length=120 )
+    web = models.URLField('Web Site Adress')
+    email_address = models.EmailField('Email Address')
+    def __str__(self):
+        return self.name
+
+
+class MyClubUser(models.Model):
+    first_name =  models.CharField( 'First Name', max_length=120 )
+    last_name =  models.CharField( 'Last Name', max_length=120 )
+    email = models.EmailField('Email User')
+    def __str__(self):
+        return self.first_name + '' + self.last_name
     
+
+class Event(models.Model):
+    name = models.CharField( 'Event Name', max_length=120 )
+    venue = models.ForeignKey(Venue, blank = True, null=True,on_delete=models.CASCADE)
+    event_date = models.DateTimeField()
+    manager = models.CharField( max_length=60 )
+    description = models.TextField( blank= True)    
+    attendees = models.ManyToManyField(MyClubUser, blank=True)
+    def __str__(self):
+        return self.name
